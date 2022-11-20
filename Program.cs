146f42bd71e51a7ac54d5a7db2bb113c82d72e60
Anton Orlov <toxa41@list.ru>
2022-11-21 01:14:42 +0300

    Создан заполняющийся массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Создать массив строк.
+// Вычислить необходимые значения.
+// Создать новый массив с необходимыми значениями.
+// Вывести на экран.
+
+Console.WriteLine("Создадим массив из нескольких значений.\nКакое колличество значений будем вводить?");
+int countArrayValue = int.Parse(Console.ReadLine());
+
+string [] startArray = new string [countArrayValue];
+ 
+ string [] FillArray (string [] array)
+ {
+	for (int i = 0; i < countArrayValue; i++)
+	{
+		Console.WriteLine($"Введите {i} значение:");
+		array[i] = Console.ReadLine();
+	}
+	return array;
+}
